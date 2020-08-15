7d9524f9eeed119e3d2ece8bfc55b265f684030e
Matthias <matthias.cartel@gmail.com>
2020-08-15 15:32:06 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'MyFormaGreenProject';
+  title = 'MyFormaGreenProject test';
 }
