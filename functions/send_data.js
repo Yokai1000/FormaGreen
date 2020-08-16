e4c97a535743e31d6cfcc145116c31293ab1a561
Matthias <matthias.cartel@gmail.com>
2020-08-16 16:30:43 +0200

    Generate service account key

--- functions/send_data.js
+++ functions/send_data.js
@@ -1,6 +1,9 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../serviceAccountKey.json");
+// Récupérer la clé privée du compte service de firebase (Accès dans les paramètres du projet)
+// renomer la clé en serviceAccountKey.json
+// déplacer la clé à la racine du projet MyFormaGreenProject
+var serviceAccount = require("../serviceAccountKey.json"); 
 
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
