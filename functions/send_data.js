f0b3e4a84e436171f2365bdeb528be04c2025cb6
Matthias <matthias.cartel@gmail.com>
2020-08-16 16:27:16 +0200

    fix send data function

--- functions/send_data.js
+++ functions/send_data.js
@@ -1,7 +1,15 @@
 var admin = require("firebase-admin");
 
-module.exports = function send_dummy_data() {
-  admin.ref("test/5").set({
-    username: "dummy"
-  });
-};
+var serviceAccount = require("../serviceAccountKey.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://formagreen-80e62.firebaseio.com"
+});
+
+var db = admin.database();
+db.ref("test/5").set({
+  username: "dummy"
+});
+
